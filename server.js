58f99fa1253468b9acd33f87ce214dac83151748
Enoch <enochstrok@outlook.com>
2021-04-02 01:37:47 -0700

    put process.env.PORT back into use

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 9000;
+const PORT = process.env.PORT || 9000;
 
 app.use(cors());
 app.use(express.json());
